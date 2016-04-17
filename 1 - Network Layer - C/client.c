0bdbeab1a5ed039d423402a6c4eca431c1945cbd
Luiz Chagas Jr <luizchagasjr@gmail.com>
2016-04-17 15:45:23 -0300

    client/server

--- 1 - Network Layer - C/client.c
+++ 1 - Network Layer - C/client.c
@@ -23,39 +23,39 @@ struct Frame{
     unsigned char checksum[4];
 };
 
-void prepareFrame(char destinationIP[16], char *message, Frame *frame){
-    frame.destination = // get MAC address from IP
-    frame.message = message;
+void prepareFrame(char destinationIP[16], char message[494], struct Frame *frame){
+    // strcpy(frame->destination,'123123123');// get MAC address from IP
+    // strcpy(frame->data,message);
 }
 
-void sendFrame(Frame *frame, int socket){
-    if (send(socket, *frame, 512, 0) < 0) // returns -1 if error
-    {
-        fprintf(stderr, "Error on sending Frame --> %s", strerror(errno));
-        exit(EXIT_FAILURE);
-    }
+void sendFrame(struct Frame *frame, int socket){
+    // if (send(socket, *frame, 512, 0) < 0) // returns -1 if error
+    // {
+    //     fprintf(stderr, "Error on sending Frame --> %s", strerror(errno));
+    //     exit(EXIT_FAILURE);
+    // }
 }
 
 int main(int argc, char **argv)
 {
         ssize_t len;
-        char buffer[256];
-        int file_size;
-        FILE *received_file;
+        char buffer[512];
+        int frame_size = 512;
+        int sent_bytes = 0;
         int remain_data = 0;
-        char *filename;
+        char *message;
         int port_number;
         int client_socket;
         struct sockaddr_in remote_addr;
 
-        if(argc != 3){
-          fprintf(stderr, "Usage: client path-to-file port-number ip\n");
+        if(argc != 4){
+          fprintf(stderr, "Usage: client message ip port\n");
           exit(EXIT_FAILURE);
         }
 
-        filename = argv[1];
+        message = argv[1];
 
-        port_number = atoi(argv[2]);
+        port_number = atoi(argv[3]);
 
         /* Zeroing remote_addr struct */
         memset(&remote_addr, 0, sizeof(remote_addr));
@@ -69,46 +69,73 @@ int main(int argc, char **argv)
         client_socket = socket(AF_INET, SOCK_STREAM, 0);
         if (client_socket == -1)
         {
-                fprintf(stderr, "Error creating socket --> %s\n", strerror(errno));
-                exit(EXIT_FAILURE);
+            fprintf(stderr, "Error creating socket --> %s\n", strerror(errno));
+            exit(EXIT_FAILURE);
+        }else{
+            fprintf(stdout, "Socket created successfully.\n");
         }
 
         /* Connect to the server */
         if (connect(client_socket, (struct sockaddr *)&remote_addr, sizeof(struct sockaddr)) == -1)
         {
-                fprintf(stderr, "Error on connect --> %s\n", strerror(errno));
-                exit(EXIT_FAILURE);
+            fprintf(stderr, "Error on connect --> %s\n", strerror(errno));
+            exit(EXIT_FAILURE);
+        }else{
+            fprintf(stdout, "Connection to server established on port %d.\n", port_number);
         }
 
-        /* Receiving file size */
-        int n = read(client_socket, buffer, 256);
-        printf("Read %d bytes for size\n",n);
-        file_size = atoi(buffer);
-        fprintf(stdout, "File size : %d\n", file_size);
-
-        received_file = fopen(filename, "w");
-        if (received_file == NULL)
+        /* Sending frame size */
+        sprintf(buffer, "%d", frame_size);
+        len = send(client_socket, buffer, sizeof(buffer), 0);
+        if (len < 0)
         {
-                fprintf(stderr, "Failed to open file foo --> %s\n", strerror(errno));
-                exit(EXIT_FAILURE);
+            fprintf(stderr, "Error on sending greetings --> %s", strerror(errno));
+            close(client_socket);
+            exit(EXIT_FAILURE);
+        }else{
+            fprintf(stdout, "Sent %ld bytes to the server\n", len);
+            fprintf(stdout, "TMQ negotiated --> %d bytes\n", frame_size);
         }
 
-        remain_data = file_size;
+        // /* Receiving file size */
+        // int n = read(client_socket, buffer, 256);
+        // printf("Read %d bytes for size\n",n);
+        // file_size = atoi(buffer);
+        // fprintf(stdout, "File size : %d\n", file_size);
 
-        bzero(buffer,256);
+        // received_file = fopen(filename, "w");
+        // if (received_file == NULL)
+        // {
+        //         fprintf(stderr, "Failed to open file foo --> %s\n", strerror(errno));
+        //         exit(EXIT_FAILURE);
+        // }
+        //
+        // remain_data = file_size;
+        //
+        // bzero(buffer,256);
+        //
+        // while (remain_data > 0)
+        // {
+        //         len = read(client_socket, buffer, 256);
+        //         fwrite(buffer, sizeof(char), len, received_file);
+        //         remain_data -= len;
+        //         fprintf(stdout, "Received %d bytes and we hope to receive : %d bytes\n", (int) len, remain_data);
+        //         if(len == 0){
+        //           close(client_socket);
+        //           exit(EXIT_FAILURE);
+        //         }
+        // }
+        // fclose(received_file);
 
-        while (remain_data > 0)
-        {
-                len = read(client_socket, buffer, 256);
-                fwrite(buffer, sizeof(char), len, received_file);
-                remain_data -= len;
-                fprintf(stdout, "Received %d bytes and we hope to receive : %d bytes\n", (int) len, remain_data);
-                if(len == 0){
-                  close(client_socket);
-                  exit(EXIT_FAILURE);
-                }
-        }
-        fclose(received_file);
+        /* Sending frame */
+        remain_data = 512;
+
+        // while (remain_data > 0)
+        // {
+        //     sent_bytes = send(client_socket, message, NULL, 512);
+        //     remain_data -= sent_bytes;
+        //     fprintf(stdout, "1. Server sent %d bytes from file's data, remaining data = %d\n", sent_bytes, remain_data);
+        // }
 
         close(client_socket);
 
--- 1 - Network Layer - C/server.c
+++ 1 - Network Layer - C/server.c
@@ -20,24 +20,18 @@ int main(int argc, char **argv)
         struct sockaddr_in server_addr;
         struct sockaddr_in client_addr;
         int fd;
-        //int sent_bytes = 0;
         int frame_size;
         FILE *received_frame;
-        //struct stat file_stat;
-        //int offset;
         int remain_data;
-        //char *file_to_send;
         int port_number;
         char buffer[512];
 
-        if(argc != 3){
-          fprintf(stderr, "Usage: server path-to-folder port_number\n");
+        if(argc != 2){
+          fprintf(stderr, "Usage: server port_number\n");
           exit(EXIT_FAILURE);
         }
 
-        //file_to_send = argv[1];
-
-        port_number = atoi(argv[2]);
+        port_number = atoi(argv[1]);
 
         /* Create server socket */
         server_socket = socket(AF_INET, SOCK_STREAM, 0);
@@ -71,7 +65,7 @@ int main(int argc, char **argv)
             fprintf(stderr, "Error on listen --> %s", strerror(errno));
             exit(EXIT_FAILURE);
         }else{
-            fprintf(stdout, "Server is now listening.\n");
+            fprintf(stdout, "Server is now listening on port %d.\n",port_number);
         }
 
         // fd = open(file_to_send, O_RDONLY);
@@ -92,65 +86,66 @@ int main(int argc, char **argv)
 
         sock_len = sizeof(struct sockaddr_in);
         /* Accepting incoming clients */
-        client_socket = accept(server_socket, (struct sockaddr *)&client_addr, &sock_len);
-        if (client_socket == -1)
-        {
-                fprintf(stderr, "Error on accept --> %s", strerror(errno));
+        while(1){
+            client_socket = accept(server_socket, (struct sockaddr *)&client_addr, &sock_len);
+            if (client_socket == -1)
+            {
+                    fprintf(stderr, "Error on accept --> %s", strerror(errno));
+                    exit(EXIT_FAILURE);
+            }
+            fprintf(stdout, "Accept client --> %s\n", inet_ntoa(client_addr.sin_addr));
+
+            //sprintf(file_size, "%d", (int) file_stat.st_size);
+
+            // /* Sending file size */
+            // len = send(client_socket, file_size, sizeof(file_size), 0);
+            // if (len < 0)
+            // {
+            //       fprintf(stderr, "Error on sending greetings --> %s", strerror(errno));
+            //       exit(EXIT_FAILURE);
+            // }
+
+            //fprintf(stdout, "Server sent %d bytes for the size\n", (int) len);
+
+            //offset = 0;
+            //remain_data = file_stat.st_size;
+
+            // /* Sending file data */
+            // while (remain_data > 0)
+            // {
+            //         sent_bytes = sendfile(client_socket, fd, NULL, 256);
+            //         remain_data -= sent_bytes;
+            //         fprintf(stdout, "1. Server sent %d bytes from file's data, offset is now : %d and remaining data = %d\n", sent_bytes, offset, remain_data);
+            // }
+
+            // Receiving frame size
+            int n = read(client_socket, buffer, sizeof(buffer));
+            printf("Read %d bytes from client\n",n);
+            frame_size = atoi(buffer);
+            fprintf(stdout, "TMQ Negotiated --> %d bytes\n", frame_size);
+
+            // Receiving frame
+            received_frame = fopen("ReceivedFrame", "w");
+            if (received_frame == NULL)
+            {
+                fprintf(stderr, "Failed to open file foo --> %s\n", strerror(errno));
                 exit(EXIT_FAILURE);
+            }
+
+            remain_data = frame_size;
+
+            bzero(buffer, frame_size);
+
+            // while (remain_data > 0)
+            // {
+            //     len = read(client_socket, buffer, frame_size);
+            //     fwrite(buffer, sizeof(char), len, received_frame);
+            //     remain_data -= len;
+            //     fprintf(stdout, "Received %d bytes and we expect to receive : %d bytes\n", (int) len, remain_data);
+            // }
+            fclose(received_frame);
+            close(client_socket);
         }
-        fprintf(stdout, "Accept client --> %s\n", inet_ntoa(client_addr.sin_addr));
-
-        //sprintf(file_size, "%d", (int) file_stat.st_size);
-
-        // /* Sending file size */
-        // len = send(client_socket, file_size, sizeof(file_size), 0);
-        // if (len < 0)
-        // {
-        //       fprintf(stderr, "Error on sending greetings --> %s", strerror(errno));
-        //       exit(EXIT_FAILURE);
-        // }
-
-        //fprintf(stdout, "Server sent %d bytes for the size\n", (int) len);
-
-        //offset = 0;
-        //remain_data = file_stat.st_size;
-
-        // /* Sending file data */
-        // while (remain_data > 0)
-        // {
-        //         sent_bytes = sendfile(client_socket, fd, NULL, 256);
-        //         remain_data -= sent_bytes;
-        //         fprintf(stdout, "1. Server sent %d bytes from file's data, offset is now : %d and remaining data = %d\n", sent_bytes, offset, remain_data);
-        // }
-
-        // Receiving frame size
-        int n = read(client_socket, buffer, 64);
-        printf("Read %d bytes for size\n",n);
-        frame_size = atoi(buffer);
-        fprintf(stdout, "Frame size : %d\n", frame_size);
-
-        // Receiving frame
-        received_frame = fopen("ReceivedFrame", "w");
-        if (received_frame == NULL)
-        {
-            fprintf(stderr, "Failed to open file foo --> %s\n", strerror(errno));
-            exit(EXIT_FAILURE);
-        }
-
-        remain_data = frame_size;
-
-        bzero(buffer, frame_size);
-
-        while (remain_data > 0)
-        {
-            len = read(client_socket, buffer, frame_size);
-            fwrite(buffer, sizeof(char), len, received_frame);
-            remain_data -= len;
-            fprintf(stdout, "Received %d bytes and we expect to receive : %d bytes\n", (int) len, remain_data);
-        }
-        fclose(received_frame);
-
-        close(client_socket);
         close(server_socket);
 
         return 0;
