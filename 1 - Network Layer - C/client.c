db0b5648baebc6703e71b6f51c8bbee9d70a1403
Larissa Barra <lari_conde94@hotmail.com>
2016-04-29 21:09:32 -0300

    Server and client updated to send and receive multiple packets in the same connection

--- 1 - Network Layer - C/client.c
+++ 1 - Network Layer - C/client.c
@@ -24,33 +24,29 @@
 #include <linux/if_arp.h>
 
 struct Frame{
-    // HEADER
-    unsigned char source[6]; // source's MAC address
-    unsigned char destination[6]; // destination's MAC address
-    unsigned char type[2];// type of protocol - 0x0800 for IPv4
-    // DATA
-    unsigned char data[494]; // 494 bytes limit for data
-    // FOOTER
-    unsigned char checksum[4];
+	// HEADER
+	unsigned char source[6]; // source's MAC address
+	unsigned char destination[6]; // destination's MAC address
+	unsigned char type[2];// type of protocol - 0x0800 for IPv4
+	// DATA
+	unsigned char data[494]; // 494 bytes limit for data
+	// FOOTER
+	unsigned char checksum[4];
 };
 
 void prepareFrame(struct Frame *frame, char *message, char* src_mac, char* dst_mac){
-    strcpy(frame->source, src_mac);
-    strcpy(frame->destination, dst_mac);
-    strcpy(frame->type, "\x08\x00");
-    sprintf(frame->data, "%-494s", message);
-    strcpy(frame->checksum, "\x00\x20\x20\x3a");
+	strcpy(frame->source, src_mac);
+	strcpy(frame->destination, dst_mac);
+	strcpy(frame->type, "\x08\x00");
+	sprintf(frame->data, "%-494s", message);
+	strcpy(frame->checksum, "\x00\x20\x20\x3a");
 }
 
 void sendFrame(struct Frame *frame, int socket, ssize_t frame_size){
-    ssize_t sent_bytes;
-    int remain_data = (int) frame_size;
-    while (remain_data > 0)
-    {
-         sent_bytes = send(socket, frame, frame_size, 0);
-         remain_data -= sent_bytes;
-         fprintf(stdout, "Client sent %d bytes to the server, remaining data = %d\n", (int) sent_bytes, remain_data);
-    }
+	ssize_t sent_bytes;
+	sent_bytes = send(socket, frame, frame_size, 0);
+	fprintf(stdout, "Client sent %d bytes to the server\n", (int) sent_bytes);
+
 }
 
 void getMacFromIp(char* ip, char* dst_mac) {
@@ -75,112 +71,113 @@ void getMacFromIp(char* ip, char* dst_mac) {
 }
 
 int main(int argc, char **argv) {
-        ssize_t len;
-        char buffer[512];
-        int frame_size = 512;
-        char *message;
-        char *ip;
-        int port_number;
-        int client_socket;
-        struct sockaddr_in remote_addr;
-        struct Frame frame1;
-        struct ifreq ifr;
-		int ifindex = 0;     /*Ethernet Interface index*/
-		int i;
-		unsigned char src_mac[6], dst_mac[6];
+	ssize_t len;
+	char buffer[512];
+	int frame_size = 512;
+	char *message;
+	char *ip;
+	int port_number;
+	int client_socket;
+	struct sockaddr_in remote_addr;
+	struct Frame frame1;
+	struct ifreq ifr;
+	int ifindex = 0;     /*Ethernet Interface index*/
+	int i;
+	unsigned char src_mac[6], dst_mac[6];
+	FILE* p;
+	char file_path[30];
+	char out[494];
+	char c[1];
+	if(argc != 4){
+		fprintf(stderr, "Usage: client message ip port\n");
+		exit(EXIT_FAILURE);
+	}
 
-        if(argc != 4){
-          fprintf(stderr, "Usage: client message ip port\n");
-          exit(EXIT_FAILURE);
-        }
+	message = argv[1];
+	ip = argv[2];
+	port_number = atoi(argv[3]);
 
-        message = argv[1];
-        ip = argv[2];
-        port_number = atoi(argv[3]);
+	/* Zeroing remote_addr struct */
+	memset(&remote_addr, 0, sizeof(remote_addr));
 
-        /* Zeroing remote_addr struct */
-        memset(&remote_addr, 0, sizeof(remote_addr));
+	/* Construct remote_addr struct */
+	remote_addr.sin_family = AF_INET;
+	inet_pton(AF_INET, "127.0.0.1", &(remote_addr.sin_addr));
+	remote_addr.sin_port = htons(port_number);
 
-        /* Construct remote_addr struct */
-        remote_addr.sin_family = AF_INET;
-        inet_pton(AF_INET, "127.0.0.1", &(remote_addr.sin_addr));
-        remote_addr.sin_port = htons(port_number);
+	/* Create client socket */
+	client_socket = socket(AF_INET, SOCK_STREAM, 0);
+	if (client_socket == -1) {
+		fprintf(stderr, "Error creating socket --> %s\n", strerror(errno));
+		exit(EXIT_FAILURE);
+	}else{
+		fprintf(stdout, "Socket created successfully.\n");
+	}
 
-        /* Create client socket */
-        client_socket = socket(AF_INET, SOCK_STREAM, 0);
-        if (client_socket == -1) {
-            fprintf(stderr, "Error creating socket --> %s\n", strerror(errno));
-            exit(EXIT_FAILURE);
-        }else{
-            fprintf(stdout, "Socket created successfully.\n");
-        }
+	/*retrieve ethernet interface index*/
+	strncpy(ifr.ifr_name, "eth0", IFNAMSIZ);
+	if (ioctl(client_socket, SIOCGIFINDEX, &ifr) == -1) {
+		perror("SIOCGIFINDEX");
+	exit(1);
+	}
 
-        /*retrieve ethernet interface index*/
-		strncpy(ifr.ifr_name, "eth0", IFNAMSIZ);
-		if (ioctl(client_socket, SIOCGIFINDEX, &ifr) == -1) {
-		    perror("SIOCGIFINDEX");
-		    exit(1);
-		}
-		ifindex = ifr.ifr_ifindex;
-
+	ifindex = ifr.ifr_ifindex;
 		/*retrieve corresponding MAC*/
-		if (ioctl(client_socket, SIOCGIFHWADDR, &ifr) == -1) {
-		    perror("SIOCGIFHWADDR");
-		    exit(1);
+	if (ioctl(client_socket, SIOCGIFHWADDR, &ifr) == -1) {
+		perror("SIOCGIFHWADDR");
+		exit(1);
+	}
+	for (i = 0; i < 6; i++) {
+		src_mac[i] = ifr.ifr_hwaddr.sa_data[i];
+	}
+	printf("Successfully got our MAC address: %02X:%02X:%02X:%02X:%02X:%02X\n",
+		src_mac[0],src_mac[1],src_mac[2],src_mac[3],src_mac[4],src_mac[5]);
+
+	getMacFromIp(ip, &dst_mac);
+	printf("Successfully got destination's MAC address: %02X:%02X:%02X:%02X:%02X:%02X\n",
+		dst_mac[0],dst_mac[1],dst_mac[2],dst_mac[3],dst_mac[4],dst_mac[5]);
+
+	/* Connect to the server */
+	if (connect(client_socket, (struct sockaddr *)&remote_addr, sizeof(struct sockaddr)) == -1) {
+		fprintf(stderr, "Error on connect --> %s\n", strerror(errno));
+		exit(EXIT_FAILURE);
+	}else{
+		fprintf(stdout, "Connection to server established on port %d.\n", port_number);
+	}
+
+		/* Sending frame size */
+	sprintf(buffer, "%d", frame_size);
+	len = send(client_socket, buffer, sizeof(buffer), 0);
+	if (len < 0) {
+		fprintf(stderr, "Error on sending greetings --> %s", strerror(errno));
+		close(client_socket);
+		exit(EXIT_FAILURE);
+	}else{
+		fprintf(stdout, "Sent %ld bytes to the server\n", len);
+		fprintf(stdout, "TMQ negotiated --> %d bytes\n", frame_size);
+	}
+
+	if (!(p = fopen(message,"r"))){
+		printf("Error To Open File\n");
+	}
+	else{
+		while(!feof(p)){
+			while (!feof(p) && i<494) {
+				c[0] = getc(p);
+				out[i] = c[0];
+				i++;
+			}
+			message = out;
+			//Prepare Frame
+			prepareFrame(&frame1, message, src_mac, dst_mac);
+			//Send Frame
+			sendFrame(&frame1, client_socket, frame_size);
+			i=0;
 		}
-		for (i = 0; i < 6; i++) {
-		    src_mac[i] = ifr.ifr_hwaddr.sa_data[i];
-		}
-		printf("Successfully got our MAC address: %02X:%02X:%02X:%02X:%02X:%02X\n",
-		    src_mac[0],src_mac[1],src_mac[2],src_mac[3],src_mac[4],src_mac[5]);
+		fclose(p);
+	}
 
-		getMacFromIp(ip, &dst_mac);
-		printf("Successfully got destination's MAC address: %02X:%02X:%02X:%02X:%02X:%02X\n",
-		    dst_mac[0],dst_mac[1],dst_mac[2],dst_mac[3],dst_mac[4],dst_mac[5]);
+	close(client_socket);
 
-
-        /* Connect to the server */
-        if (connect(client_socket, (struct sockaddr *)&remote_addr, sizeof(struct sockaddr)) == -1) {
-            fprintf(stderr, "Error on connect --> %s\n", strerror(errno));
-            exit(EXIT_FAILURE);
-        }else{
-            fprintf(stdout, "Connection to server established on port %d.\n", port_number);
-        }
-
-        /* Sending frame size */
-        sprintf(buffer, "%d", frame_size);
-        len = send(client_socket, buffer, sizeof(buffer), 0);
-        if (len < 0) {
-            fprintf(stderr, "Error on sending greetings --> %s", strerror(errno));
-            close(client_socket);
-            exit(EXIT_FAILURE);
-        }else{
-            fprintf(stdout, "Sent %ld bytes to the server\n", len);
-            fprintf(stdout, "TMQ negotiated --> %d bytes\n", frame_size);
-        }
-
-        //I'm not going to deal with messages over 512 bytes
-        // if(strlen(message)>512){
-        //     int i = 0;
-        //     while(i+512 < strlen(message)){
-        //         //Prepare Frame
-        //         prepareFrame(&frame1, message);
-        //         //Send Frame
-        //         sendFrame(&frame1, client_socket, frame_size);
-        //         i += 512;
-        //     }
-        // }else{
-        //     //Prepare Frame
-        //     prepareFrame(&frame1, message);
-        //     //Send Frame
-        //     sendFrame(&frame1, client_socket, frame_size);
-        // }
-        //Prepare Frame
-        prepareFrame(&frame1, message, src_mac, dst_mac);
-        //Send Frame
-        sendFrame(&frame1, client_socket, frame_size);
-
-        close(client_socket);
-
-        return 0;
+	return 0;
 }
--- 1 - Network Layer - C/server.c
+++ 1 - Network Layer - C/server.c
@@ -1,116 +1,109 @@
 #include <stdio.h>
-#include <sys/types.h>
-#include <sys/socket.h>
 #include <stdlib.h>
 #include <errno.h>
 #include <string.h>
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <netinet/in.h>
-#include <sys/stat.h>
 #include <fcntl.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
 #include <sys/sendfile.h>
 
-int main(int argc, char **argv)
-{
-        int server_socket;
-        int client_socket;
-        socklen_t sock_len;
-        ssize_t len;
-        struct sockaddr_in server_addr;
-        struct sockaddr_in client_addr;
-        int fd;
-        int frame_size;
-        FILE *received_frame;
-        int remain_data;
-        int port_number;
-        char buffer[512];
-        char filename[18];
-        int i = 0;
+int main(int argc, char **argv){
+	int server_socket;
+	int client_socket;
+	socklen_t sock_len;
+	ssize_t len;
+	struct sockaddr_in server_addr;
+	struct sockaddr_in client_addr;
+	int fd;
+	int frame_size;
+	FILE *received_frame;
+	int port_number;
+	char buffer[512];
+	char filename[18];
+	int i = 0;
 
-        if(argc != 2){
-          fprintf(stderr, "Usage: server port_number\n");
-          exit(EXIT_FAILURE);
-        }
+	if(argc != 2){
+		fprintf(stderr, "Usage: server port_number\n");
+		exit(EXIT_FAILURE);
+	}
 
-        port_number = atoi(argv[1]);
+	port_number = atoi(argv[1]);
 
-        /* Create server socket */
-        server_socket = socket(AF_INET, SOCK_STREAM, 0);
-        if (server_socket == -1)
-        {
-            fprintf(stderr, "Error creating socket --> %s", strerror(errno));
-            exit(EXIT_FAILURE);
-        }else{
-            fprintf(stdout, "Socket created successfully.\n");
-        }
+	/* Create server socket */
+	server_socket = socket(AF_INET, SOCK_STREAM, 0);
+	if (server_socket == -1){
+		fprintf(stderr, "Error creating socket --> %s", strerror(errno));
+		exit(EXIT_FAILURE);
+	}else{
+		fprintf(stdout, "Socket created successfully.\n");
+	}
 
-        /* Zeroing server_addr struct */
-        memset(&server_addr, 0, sizeof(server_addr));
-        /* Construct server_addr struct */
-        server_addr.sin_family = AF_INET;
-        inet_pton(AF_INET, "127.0.0.1", &(server_addr.sin_addr));
-        server_addr.sin_port = htons(port_number);
+	/* Zeroing server_addr struct */
+	memset(&server_addr, 0, sizeof(server_addr));
 
-        /* Bind */
-        if ((bind(server_socket, (struct sockaddr *)&server_addr, sizeof(struct sockaddr))) == -1)
-        {
-            fprintf(stderr, "Error on bind --> %s", strerror(errno));
-            exit(EXIT_FAILURE);
-        }else{
-            fprintf(stdout, "Bind successful.\n");
-        }
+	/* Construct server_addr struct */
+	server_addr.sin_family = AF_INET;
+	inet_pton(AF_INET, "127.0.0.1", &(server_addr.sin_addr));
+	server_addr.sin_port = htons(port_number);
 
-        /* Listening to incoming connections */
-        if ((listen(server_socket, 5)) == -1)
-        {
-            fprintf(stderr, "Error on listen --> %s", strerror(errno));
-            exit(EXIT_FAILURE);
-        }else{
-            fprintf(stdout, "Server is now listening on port %d.\n",port_number);
-        }
+	/* Bind */
+	if ((bind(server_socket, (struct sockaddr *)&server_addr, sizeof(struct sockaddr))) == -1){
+		fprintf(stderr, "Error on bind --> %s", strerror(errno));
+		exit(EXIT_FAILURE);
+	}else{
+		fprintf(stdout, "Bind successful.\n");
+	}
+	/* Listening to incoming connections */
+	if ((listen(server_socket, 5)) == -1){
+		fprintf(stderr, "Error on listen --> %s", strerror(errno));
+		exit(EXIT_FAILURE);
+	}else{
+		fprintf(stdout, "Server is now listening on port %d.\n",port_number);
+	}
 
-        sock_len = sizeof(struct sockaddr_in);
-        /* Accepting incoming clients */
-        while(1){
-            client_socket = accept(server_socket, (struct sockaddr *)&client_addr, &sock_len);
-            if (client_socket == -1)
-            {
-                    fprintf(stderr, "Error on accept --> %s", strerror(errno));
-                    exit(EXIT_FAILURE);
-            }
-            fprintf(stdout, "Accept client --> %s\n", inet_ntoa(client_addr.sin_addr));
+	sock_len = sizeof(struct sockaddr_in);
+	/* Accepting incoming clients */
+	while(1){
+		client_socket = accept(server_socket, (struct sockaddr *)&client_addr, &sock_len);
+		if (client_socket == -1){
+			fprintf(stderr, "Error on accept --> %s", strerror(errno));
+			exit(EXIT_FAILURE);
+		}
+		fprintf(stdout, "Accept client --> %s\n", inet_ntoa(client_addr.sin_addr));
 
-            // Receiving frame size
-            int n = read(client_socket, buffer, sizeof(buffer));
-            printf("Read %d bytes from client\n",n);
-            frame_size = atoi(buffer);
-            fprintf(stdout, "TMQ Negotiated --> %d bytes\n", frame_size);
+		// Receiving frame size
+		int n = read(client_socket, buffer, sizeof(buffer));
+		printf("Read %d bytes from client\n",n);
+		frame_size = atoi(buffer);
+		fprintf(stdout, "TMQ Negotiated --> %d bytes\n", frame_size);
 
-            // Receiving frame
-            sprintf(filename, "ReceivedFrame%05d",i++);
-            received_frame = fopen(filename, "w");
-            if (received_frame == NULL)
-            {
-                fprintf(stderr, "Failed to open file foo --> %s\n", strerror(errno));
-                exit(EXIT_FAILURE);
-            }
+		while(1){
+			bzero(buffer, frame_size);
 
-            remain_data = frame_size;
+			// Receiving frame
+			len = read(client_socket, buffer, frame_size);
+			if((int)len <= 0) break;
 
-            bzero(buffer, frame_size);
+			sprintf(filename, "ReceivedFrame%05d",i++);
+			received_frame = fopen(filename, "w");
+			if (received_frame == NULL){
+				fprintf(stderr, "Failed to open file foo --> %s\n", strerror(errno));
+				exit(EXIT_FAILURE);
+			}
+			fwrite(buffer, sizeof(char), len, received_frame);
 
-            while (remain_data > 0)
-            {
-                len = read(client_socket, buffer, frame_size);
-                fwrite(buffer, sizeof(char), len, received_frame);
-                remain_data -= len;
-                fprintf(stdout, "Server received %d bytes.\n", (int) len);
-            }
-            fclose(received_frame);
-            close(client_socket);
-        }
-        close(server_socket);
+			fprintf(stdout, "Server received %d bytes.\n", (int) len);
 
-        return 0;
+			fclose(received_frame);
+		}
+		close(client_socket);
+	}
+	close(server_socket);
+
+	return 0;
 }
