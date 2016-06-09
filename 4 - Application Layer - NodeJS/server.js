884674e92380b7227f35dca7f358729e53255230
Luiz Chagas Jr <luizchagasjr@gmail.com>
2016-06-09 00:31:14 -0300

    http server

--- 4 - Application Layer - NodeJS/server.js
+++ 4 - Application Layer - NodeJS/server.js
@@ -1,8 +1,42 @@
-var express = require('express'),
-    app = express();
+var net = require('net');
+var fs = require('fs');
 
-app.use(express.static(__dirname + '/www'));
+var HOST = '127.0.0.1';
+var PORT = 8080;
 
-app.listen(8000);
+// Create a server instance, and chain the listen function to it
+// The function passed to net.createServer() becomes the event handler for the 'connection' event
+// The sock object the callback function receives UNIQUE for each connection
+net.createServer(function(sock) {
 
-console.log('Server running on port 8000');
+    var requestedFile;
+    var header;
+    var body;
+
+    // We have a connection - a socket object is assigned to the connection automatically
+    console.log('Client connected: ' + sock.remoteAddress +':'+ sock.remotePort + "\n\n");
+
+    // Add a 'data' event handler to this instance of socket
+    sock.on('data', function(data) {
+
+        console.log(data.toString());
+
+        header = "HTTP/1.1 200 OK\r\n"
+            + "Content-Type: text/html;charset=UTF-8\r\n"
+            + "\r\n";
+
+        body = fs.readFileSync('index.html', 'utf8');
+
+        // Write data back to the socket
+        sock.write(header + body);
+        sock.destroy();
+    });
+
+    // Add a 'close' event handler to this instance of socket
+    sock.on('close', function(data) {
+        console.log('Closed connection\n\n');
+    });
+
+}).listen(PORT, HOST);
+
+console.log('Server listening on ' + HOST +':'+ PORT);
